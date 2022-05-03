c0d8b9132b10011f5771a0d4c3534ef0c9fad986
latenighticecream <latenighticecream@github.io>
2022-05-03 19:25:15 +0200

    gtk theme added

--- .scripts/applyColorTemplates.sh
+++ .scripts/applyColorTemplates.sh
@@ -7,3 +7,10 @@ INJECTOR=~/.scripts/colorInjector.py
 "$INJECTOR" ~/.config/kitty/kitty_colors.conf "$TEMPLATE_DIR/kitty/kitty_colors.template" "$CONFIG_FILE"
 "$INJECTOR" ~/.Xresources "$TEMPLATE_DIR/Xresources.template" "$CONFIG_FILE"
 "$INJECTOR" ~/.config/rofi/colors.rasi "$TEMPLATE_DIR/rofi/colors.rasi.template" "$CONFIG_FILE"
+"$INJECTOR" ~/.local/share/themes/Colloid-gtk-theme-template/src/sass/_color-palette-default.scss "$TEMPLATE_DIR/gtk/_color-palette-default.scss.template" "$CONFIG_FILE"
+cd ~/.local/share/themes/Colloid-gtk-theme-template/
+pwd
+~/.local/share/themes/Colloid-gtk-theme-template/build.sh
+~/.local/share/themes/Colloid-gtk-theme-template/install.sh --tweaks rimless
+cd
+#"$INJECTOR" ~/.templates/gtk/_color-palette-default.scss "$TEMPLATE_DIR/gtk/_color-palette-default.scss.template" "$CONFIG_FILE"
