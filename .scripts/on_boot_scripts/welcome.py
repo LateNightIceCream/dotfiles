0b641f7e28ab5bec52a0ec437319a93cf3fa73b8
latenighticecream <latenighticecream@github.io>
2023-08-17 15:55:11 +0200

    add scripts

--- .scripts/on_boot_scripts/welcome.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-import subprocess
-
-os.system('notify-send "Welcome!" "Hi Richard, nice to see you!"')
-
-#print('\007') # sound
--- .scripts/testNotifications.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-notify-send -u low "Low urgency notification" "Have a nice day"
-notify-send -u normal "Normal urgency notification" "Have a nice day"
-notify-send -u critical "Critical urgency notification" "Have a nice day"
